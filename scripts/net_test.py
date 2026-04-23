4e2de5f5d0eb21725640a03456d6b6e8430b70d9
于澎 <hzyupeng@corp.netease.com>
2026-04-23 16:21:57 +0800

    ds 连上了！

--- scripts/net_test.py
+++ scripts/net_test.py
@@ -1,7 +1,8 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(b"Hello UDP", ("127.0.0.1", 17777))
+# sock.sendto(b"Hello UDP", ("127.0.0.1", 17777))
+sock.sendto(b"Hello UDP", ("7.25.185.227", 7777))
 
 # 如果想接收回复
 sock.settimeout(3)
